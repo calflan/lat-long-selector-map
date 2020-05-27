8ffdd1f8bdafad8c2f8b051d003fecd1be55826f
Calum Flanagan <flanagancalum@gmail.com>
2020-05-27 16:17:29 +0100

    Add a map

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
-console.log("webpack-template-app");
+import React from "react";
+import { render } from "react-dom";
+import MyComponent from "./myComponent";
+
+var myComponentElement = document.getElementById("my-component");
+
+render(<MyComponent />, myComponentElement);
